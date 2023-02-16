d0835938d80c4d4e196f3f482533bfb03085c1e8
Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2023-02-16 14:28:08 +0800

    Update

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $(".m-close").click(function(){
+        $(".m-mobile").css("display","none");
+    });
+
+    $(".icon-mobile").click(function(){
+        $(".m-mobile").css("display","block");
+    });
+});
